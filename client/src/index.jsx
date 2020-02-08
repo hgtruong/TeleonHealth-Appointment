93c4ba1d6728de647dbaa30397b504ee56cb607b
Huy Truong <ht@Huys-MBP.attlocal.net>
2020-02-07 20:52:08 -0800

    Able to big calendar month view

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,19 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-// Need to import doctor JSON
+import doctors from "../../doctors";
+import moment from 'moment';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+const localizer = momentLocalizer(moment);
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      doctors: doctors, 
+      events: []
+    };
+  }
 
-    render() {
-        return (
-            <h2> test</h2>
-        )
-    }
+  componentDidMount() {
+    this.setEvents();
+  }
+
+  setEvents () {
+    let event =     [{ 
+      start: new Date(),
+      end: new Date(),
+      title: `test Stay (Human: test)`,
+      allDay: true
+    }]
+    this.setState({events: event});
+  }
+
+  render() {
+    return (
+      <div className="calendar-container">
+        <Calendar
+          localizer={localizer}
+          events={this.state.events}
+         />
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
