a17e23a5fef2b66dfce8215c500f89c60b69e2b2
Huy Truong <ht@Huys-MBP.attlocal.net>
2020-02-09 11:30:00 -0800

    Changed Calendar.jsx from stateful to stateless

--- client/src/components/Calendar.jsx
+++ client/src/components/Calendar.jsx
@@ -1,75 +1,17 @@
 import React from "react";
-import doctors from "../../../doctors";
 import moment from 'moment';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+
 const localizer = momentLocalizer(moment);
 
-class RBCalendar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      doctors: doctors, 
-      events: []
-    };
-  }
+const style = {
+  margin: '5%'
+}
 
-  componentDidMount() {
-    this.setEvents();
-  }
+const RBCalendar = function(props) {
 
-  setEvents () {
-    let event = [
-      { 
-        start: new Date('Febuary 7 2020'),
-        end: new Date('Febuary 7 2020'),
-        title: `Counseling with Dr. Howser`,
-        allDay: true,
-        doctor: {  
-          "firstName": "Doogie",
-          "lastName": "Howser",
-          "occupation": "Psychologist",
-          "calendarColor": "GREEN"
-        }
-      },
-      {
-        start: new Date(),
-        end: new Date(),
-        title: `Diagnosis with Dr. House`,
-        allDay: true,
-        doctor: {  
-          "firstName": "Gregory",
-          "lastName": "House",
-          "occupation": "Diagnostic Medicine",
-          "calendarColor": "BLUE"
-        }
-      },
-      {
-        start: new Date('Febuary 3 2020'),
-        end: new Date('Febuary 3 2020'),
-        title: `Psychiatry with Dr. Crane`,
-        allDay: true,
-        doctor: {
-          "firstName": "Frasier",
-          "lastName": "Crane",
-          "occupation": "Psychiatrist",
-          "calendarColor": "YELLOW"
-        }
-      }
-    ]
-    this.setState({events: event});
-  }
-
-  handleEventDoubleClick() {
-    console.log('event double clicked')
-  }
-
-  handleSelectSlot({start,end,action}) {
-    console.log('select lot clicked', action);
-  }
-
-  eventStyleGetter(event, start,end, isSelected) {
-    console.log('event is', event);
+  const eventStyleGetter = function (event, start,end, isSelected){
     var style = {
         backgroundColor: event.doctor.calendarColor,
         color: event.doctor.calendarColor === "YELLOW" ? 'red' : 'white',
@@ -79,22 +21,20 @@ class RBCalendar extends React.Component {
     };
   }
 
-  render() {
-    return (
-      <div className="calendar-container">
-        <Calendar
-          localizer={localizer}
-          events={this.state.events}
-          defaultView="month"
-          views={["month"]}
-          selectable={true}
-          onDoubleClickEvent={this.handleEventDoubleClick}
-          onSelectSlot={this.handleSelectSlot}
-          eventPropGetter={this.eventStyleGetter}
-         />
-      </div>
-    )
-  }
+  return (
+    <div className="calendar-container" style={style}>
+      <Calendar
+        localizer={localizer}
+        events={props.parentEvents}
+        defaultView="month"
+        views={["month"]}
+        selectable={true}
+        onDoubleClickEvent={props.doubleClickHandler}
+        onSelectSlot={props.handleSlotClick}
+        eventPropGetter={eventStyleGetter.bind(this)}
+        />
+    </div>
+  )
 }
 
 export { RBCalendar }
